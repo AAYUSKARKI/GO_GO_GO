f7ccf08be327b275f1c59e34eefd0dcf34e27b8d
AAYUS KARKI <alexkarki2060@gmail.com>
2024-11-02 20:06:12 +0545

    waitgroup

--- 20_goRoutines/main.go
+++ 20_goRoutines/main.go
@@ -2,16 +2,20 @@ package main
 
 import (
 	"fmt"
-	"time"
+	"sync"
 )
 
-func task(id int) {
+func task(id int, w *sync.WaitGroup) {
+	defer w.Done()
 	fmt.Println("Task", id)
 }
 func main() {
+	var wg sync.WaitGroup
+
 	for i := 0; i < 10; i++ {
-		go task(i)
+		wg.Add(1)
+		go task(i, &wg)
 	}
 
-	time.Sleep(1 * time.Second)
+	wg.Wait()
 }
